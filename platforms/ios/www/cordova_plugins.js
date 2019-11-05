d995e6520aee2623b321fe12ac1eb87061419406
intrsivakumar <int.rsivakumar@gmail.com>
2019-11-05 18:06:05 +0530

    Updated iOS
    Update ios

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,25 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [
-    {
-      "id": "cordova-plugin-fcm-updated.FCMPlugin",
-      "file": "plugins/cordova-plugin-fcm-updated/www/FCMPlugin.js",
-      "pluginId": "cordova-plugin-fcm-updated",
-      "clobbers": [
-        "FCMPlugin"
-      ]
-    },
-    {
-      "id": "resulticks-cordova-plugin-inh.ReCordovaPlugin",
-      "file": "plugins/resulticks-cordova-plugin-inh/www/ReCordovaPlugin.js",
-      "pluginId": "resulticks-cordova-plugin-inh",
-      "clobbers": [
-        "ReCordovaPlugin"
-      ]
-    }
-  ];
+  module.exports = [];
   module.exports.metadata = {
-    "cordova-plugin-fcm-updated": "1.0.1",
-    "cordova-plugin-whitelist": "1.3.4",
-    "resulticks-cordova-plugin-inh": "0.1.4"
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
